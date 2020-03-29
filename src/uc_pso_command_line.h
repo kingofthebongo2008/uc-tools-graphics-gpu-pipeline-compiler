7f011d5f23d6332b789566cd60667abb81426116
kingofthebongo2008 <stefan.dyulgerov@gmail.com>
2020-03-29 21:19:17 +0300

    fixes

--- src/uc_pso_command_line.h
+++ src/uc_pso_command_line.h
@@ -16,7 +16,7 @@ namespace uc
         inline std::string get_abs_path(const std::string& file_name)
         {
             namespace fs = std::filesystem;
-            return fs::canonical(fs::absolute( fs::path(file_name) ) ).string();
+            return fs::absolute(fs::path(file_name)).string();
         }
 
         inline auto get_input_value(const boost::program_options::variables_map & map, const std::string& s)
